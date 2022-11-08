f6692a5f322e2082d2159a3b2c933d4cc9ee971d
Zornitsa Stefanova <aurora.stefanova@gmail.com>
2022-11-08 18:17:51 +0200

    Zad5_Homework2

---
+++ src/test/java/SecondHomework/Zad5_Homework2.java
@@ -0,0 +1,28 @@
+package SecondHomework;
+
+public class Zad5_Homework2 {
+    public static void main(String[] arg){
+        int[] createdArray = {10, 12, 1, 8, 4};
+
+        //creating new array with the same number elements
+        int[] secondArray = new int[createdArray.length];
+        //Check length for second array
+        System.out.println("Second array length is: " + secondArray.length);
+
+        //first way to copy elements
+        secondArray = createdArray;
+
+        System.out.println("Printing Second array elements!");
+        for (int i = 0; i <secondArray.length; i++){
+            System.out.println(secondArray[i]);
+        }
+
+        //second way to copy elements from the first array
+        System.out.println("Printing Second array elements, with adding in different way!");
+        for (int j = 0; j < createdArray.length; j++){
+            secondArray[j] = createdArray[j];
+            System.out.println("Element is: " + secondArray[j]);
+
+        }
+    }
+}
