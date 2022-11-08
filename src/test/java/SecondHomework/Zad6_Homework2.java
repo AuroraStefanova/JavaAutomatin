87fe35b951f4a5df5338a513a0fb20aa2427fde0
Zornitsa Stefanova <aurora.stefanova@gmail.com>
2022-11-08 18:39:38 +0200

    Zad6_Homework2

---
+++ src/test/java/SecondHomework/Zad6_Homework2.java
@@ -0,0 +1,33 @@
+package SecondHomework;
+
+public class Zad6_Homework2 {
+    public static void main(String[] arg) {
+
+        int[] firstArray = {1, 2, 3};
+        int[] secondArray = {1, 4, 3};
+        //count is addet to check for sure are all elements is in the first if statement and to print for the end message
+        int count = 0;
+
+        //if length are different theq are not equals
+        if(firstArray.length != secondArray.length){
+            System.out.println("The array length is different and they are not equals!");
+        }
+        else{
+            for (int i = 0; i < firstArray.length; i ++){
+                if(firstArray[i] == secondArray[i]){
+                    count++;
+                    continue;
+                }
+                else {
+                    System.out.println("The arrays are not equals!");
+                    break;
+                }
+            }
+
+            if(count == firstArray.length){
+
+                System.out.println("Yes, arrays are equals!");
+            }
+        }
+    }
+}
