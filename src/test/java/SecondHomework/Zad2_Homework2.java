fe81ea671fb405d80e8f1133433520022663b58c
Zornitsa Stefanova <aurora.stefanova@gmail.com>
2022-11-08 17:02:10 +0200

    Zad2_Homework2

---
+++ src/test/java/SecondHomework/Zad2_Homework2.java
@@ -0,0 +1,25 @@
+package SecondHomework;
+
+import java.util.Scanner;
+
+public class Zad2_Homework2 {
+    public static void main(String[] arg){
+
+        Scanner scr = new Scanner(System.in);
+
+        System.out.println("Please enter from console two double numbers");
+
+        double firstNum = scr.nextDouble();
+        double secondNum = scr.nextDouble();
+
+        //calling method
+        System.out.println(" The average arithmetically from two numbers is: " + arithmeticallyAverage(firstNum, secondNum));
+    }
+    //method average arithmetically from two numbers
+    public static double arithmeticallyAverage(double firstNum, double secondNum){
+
+        double sum = ((firstNum + secondNum)/ 2);
+
+        return sum;
+    }
+}
