8e41782e9360396e87de43dc52ddfaa6c6ea9565
Zornitsa Stefanova <aurora.stefanova@gmail.com>
2022-11-08 17:22:12 +0200

    Zad3_Homework2

---
+++ src/test/java/SecondHomework/Zad3_Homework2.java
@@ -0,0 +1,25 @@
+package SecondHomework;
+
+import java.util.Scanner;
+
+public class Zad3_Homework2 {
+    public static void main(String[] arg){
+
+        Scanner scr = new Scanner(System.in);
+
+        double[] firstArrayFromTheConsole = new double[5];
+
+        System.out.println("Please enter five double numbers from the console!");
+
+        //adding elements from type double from the console
+        for (int i = 0; i < firstArrayFromTheConsole.length; i++){
+            firstArrayFromTheConsole[i] = scr.nextDouble();
+        }
+
+        // Printing array
+        for (int i = 0; i < firstArrayFromTheConsole.length; i++){
+            System.out.println( firstArrayFromTheConsole[i]);
+        }
+
+    }
+}
