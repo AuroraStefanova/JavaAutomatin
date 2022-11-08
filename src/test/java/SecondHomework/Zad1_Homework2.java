b96926114b9283ff7db5276404826b41982ce607
Zornitsa Stefanova <aurora.stefanova@gmail.com>
2022-11-08 16:43:55 +0200

    zad1_Homework2

---
+++ src/test/java/SecondHomework/Zad1_Homework2.java
@@ -0,0 +1,27 @@
+package SecondHomework;
+
+import java.util.Scanner;
+
+public class Zad1_Homework2 {
+    public static void main(String[] arg){
+
+        Scanner scr = new Scanner(System.in);
+
+        System.out.println("Please enter a two integer numbers!");
+
+        int firstNum = scr.nextInt();
+        int secondNum = scr.nextInt();
+
+        System.out.println("The sum from two numbers is: " + calculateTwoNumbers(firstNum, secondNum));
+
+        //calculateTwoNumbers(firstNum, secondNum);
+
+    }
+    //Method calculate two numbers
+    static int calculateTwoNumbers(int first, int second){
+
+        int sum = first + second;
+        return sum;
+
+    }
+}
